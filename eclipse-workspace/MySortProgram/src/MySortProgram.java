72ce2a18d8e391693ff0f1a102598d5d8f2a86a7
bluemeaford <bannoch@hotmail.com>
2021-12-09 13:14:32 -0500

    src

---
+++ eclipse-workspace/MySortProgram/src/MySortProgram.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class MySortProgram {
+
+	public static void main(String[] args) {
+	int[]my_array1= {99,22,55,77,33};
+	String[]my_array2={"Bobby,Bagheera,James,Maya,Mango"};
+	System.out.println("Sorted numeric array:"+Arrays.toString(my_array1));
+	System.out.println("Sorted string array:"+Arrays.toString(my_array2));
+	}
+
+	}
+
+
